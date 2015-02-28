d4d9fc3c6fad2aacae404a04b333b572128660c9
Samuel <pythonners@gmail.com>
2015-02-27 23:59:49 -0500

    Publicando primera vercion de cristo viene ya

---
+++ client/inicio/index.js
@@ -0,0 +1,20 @@
+Template.index.rendered = function(){
+  /* ======= ScrollTo ======= */
+  $('a.scrollto').on('click', function(e){
+
+    //store hash
+    var target = this.hash;
+
+    e.preventDefault();
+
+    $('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
+    //Collapse mobile menu after clicking
+    if ($('.navbar-collapse').hasClass('in')){
+      $('.navbar-collapse').removeClass('in').addClass('collapse');
+    }
+
+  });
+
+  // activamos los complementos extras
+    $('[data-toggle="popover"]').popover();
+};
