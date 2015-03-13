04333b0bf10dd41f3213483f9d426524c2c6b677
Samuel <pythonners@gmail.com>
2015-03-13 00:10:45 -0500

    Monitoriar aplicacion con google y kadira

--- client/js/main.js
+++ client/js/main.js
@@ -1,10 +1,21 @@
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-36754134-2', 'auto');
+ga('send', 'pageview');
+
+
+
 jQuery(document).ready(function($) {
 
     /* ======= Scrollspy ======= */
     $('body').scrollspy({ target: '#header', offset: 400});
-    
+
     /* ======= Fixed header when scrolled ======= */
-    
+
     $(window).bind('scroll', function() {
          if ($(window).scrollTop() > 50) {
              $('#header').addClass('navbar-fixed-top');
@@ -13,21 +24,21 @@ jQuery(document).ready(function($) {
              $('#header').removeClass('navbar-fixed-top');
          }
     });
-   
+
     /* ======= ScrollTo ======= */
     $('a.scrollto').on('click', function(e){
-        
+
         //store hash
         var target = this.hash;
-                
+
         e.preventDefault();
-        
+
 		$('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
         //Collapse mobile menu after clicking
 		if ($('.navbar-collapse').hasClass('in')){
 			$('.navbar-collapse').removeClass('in').addClass('collapse');
 		}
-		
+
 	});
 
 });
---
+++ server/kadira.js
@@ -0,0 +1,3 @@
+// configuracion para monitoriar la plicacion
+
+Kadira.connect('XmCJwuoYokfSvcgYh', '3c0965c2-1a83-49b7-82d9-a38cbdfb1f44');
