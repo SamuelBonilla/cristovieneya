fbb55cc4688398ef3afeb5095a34357b960ff48e
Samuel <pythonners@gmail.com>
2015-06-11 20:11:50 -0500

    Comentarios con disqus

---
+++ client/blog/post/postid.js
@@ -0,0 +1,16 @@
+/**
+ * Created by samuel on 27/05/15.
+ */
+
+
+Template.postPermalink.rendered = function(){
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'cristovienemuypronto';
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+};
