42a7135221a8c5f1733e2612c5457b286fdf0ef3
Samuel <pythonners@gmail.com>
2015-07-14 21:13:31 -0500

    Agregando spinner para mejorar la esperiencia de usuario usando paginacion

--- client/blog/inicio.js
+++ client/blog/inicio.js
@@ -3,13 +3,13 @@
  */
 
 Template.blog.helpers({
-    posts: function(){
+    //posts: function(){
         /* el cliente ve los datos enviados por publish como un
          como un todo asi esten filtrados son un todo para
          el cliente, en este caso se adseden a todos los post que
          la subscipcion a enviado */
-        return Posts.find({}, {sort: {submitted: -1}});
-    },
+      //  return Posts.find({}, {sort: {submitted: -1}});
+    //},
 
     ownPost: function(){
         return this.userId === Meteor.userId();
