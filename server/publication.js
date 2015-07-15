b1b0ab09913169d60b18b3dbb5be976a100b0bf1
Samuel <pythonners@gmail.com>
2015-07-14 19:34:49 -0500

    Creando nueva ruta /blogs/:postsLimit? para limitar los articulos

--- server/publication.js
+++ server/publication.js
@@ -2,8 +2,9 @@
  * Created by samuel on 23/03/15.
  */
 
-Meteor.publish("posts", function(){
-    return Posts.find();
+Meteor.publish("posts", function(postsLimit){
+    check(postsLimit, Number);
+    return Posts.find({}, {sort : {submitted: -1}, limit : postsLimit});
 });
 
 Meteor.publish("post", function(permalink){
